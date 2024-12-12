36343aff49f4f2612a20039d2a0e87500e58a357
ascyrax <ascyrax01@gmail.com>
2024-12-12 22:08:42 +0530

    2d game complete :)

--- dtc2d/player.gd
+++ dtc2d/player.gd
@@ -1,15 +1,21 @@
 extends Area2D
 
+signal hit
 
 @export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
 
 
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
 	print(screen_size)
-	hide()
+	#hide()
 
 
 func output(dir:String, velocity:Vector2):
@@ -44,3 +50,9 @@ func _process(delta: float) -> void:
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "up"
 		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+func _on_body_entered(body: Node2D) -> void:
+	hide() # Player disappears after being hit.
+	hit.emit()
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
